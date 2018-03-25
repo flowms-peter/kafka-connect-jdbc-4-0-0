2a089b1e09dd41d2c73f7af27013abafcf7cf3c2
pwherman73 <37722713+pwherman73@users.noreply.github.com>
2018-03-25 14:12:09 -0400

    Update DbDialect.java

--- src/main/java/io/confluent/connect/jdbc/sink/dialect/DbDialect.java
+++ src/main/java/io/confluent/connect/jdbc/sink/dialect/DbDialect.java
@@ -93,10 +93,10 @@ public abstract class DbDialect {
 
     joinToBuilder(builder, ", ", nonKeyColumns, updateTransformer);
     
-    if (!updateSetAppend().isEmpty()) {
-      builder.append(", ");
-      builder.append(updateSetAppend());
-    }
+/*    if (!updateSetAppend().isEmpty()) {
+ *     builder.append(", ");
+ *     builder.append(updateSetAppend());
+*/   }
 
     if (!keyColumns.isEmpty()) {
       builder.append(" WHERE ");
@@ -104,10 +104,10 @@ public abstract class DbDialect {
 
     joinToBuilder(builder, " AND ", keyColumns, updateTransformer);
     
-    if (!updateWhereAppend().isEmpty()) {
-      builder.append(" AND ");
-      builder.append(updateWhereAppend());
-    }
+/*    if (!updateWhereAppend().isEmpty()) {
+ *     builder.append(" AND ");
+ *     builder.append(updateWhereAppend());
+*/    }
 
     return builder.toString();
   }
