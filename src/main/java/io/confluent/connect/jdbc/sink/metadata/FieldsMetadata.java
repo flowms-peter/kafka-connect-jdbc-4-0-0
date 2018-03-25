b1a9f99c85e559a331d35f5cbd418d8d43f5457b
pwherman73 <37722713+pwherman73@users.noreply.github.com>
2018-03-25 16:01:37 -0400

    Update FieldsMetadata.java

--- src/main/java/io/confluent/connect/jdbc/sink/metadata/FieldsMetadata.java
+++ src/main/java/io/confluent/connect/jdbc/sink/metadata/FieldsMetadata.java
@@ -247,7 +247,7 @@ public class FieldsMetadata {
       }
     } else {
       for (String fieldName : configuredPkFields) {
-        if (valueSchema.field(fieldName) == null) {
+        if (valueSchema.field(fieldName) == null && valueSchema.field(fieldName) != null) {
           throw new ConnectException(String.format(
               "PK mode for table '%s' is %s with configured PK fields %s, but record value "
               + "schema does not contain field: %s",
